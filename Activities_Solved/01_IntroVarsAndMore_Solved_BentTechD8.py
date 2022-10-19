d957e767f6eff82fac2ff44b08bfd448981e30c3
benttechd8 <95981277+benttechd8@users.noreply.github.com>
2022-10-18 22:29:14 -0400

    Add files via upload

---
+++ Activities_Solved/01_IntroVarsAndMore_Solved_BentTechD8.py
@@ -0,0 +1,46 @@
+#twitter @benttechd8
+#You got this!
+#Practice!
+#Bottom in seat, fingers on keys!
+
+
+# Let this rip! use print to, well, print "Hello World!"
+print("Hello World!")  #delete the line leading "#"
+
+# Variable Assignment
+# ==========================================
+# Integers, Floats, Booleans, Strings
+# add vaules to the below variables
+
+#int
+age = 40
+#float
+gramsSugar = 4.367
+#Boolean, T / F
+boolTrue = True
+#Stings
+lastName = "Schwark"
+
+# Referencing and Output (printing to screen)
+# Referencing is nothing more than calling
+# / using the variable
+# ===========================================
+# Print variable values
+print(age)
+print(gramsSugar)
+print(boolTrue)
+print(lastName)
+
+# Print types of variables
+print(type(age))
+print(type(gramsSugar))
+print(type(boolTrue))
+print(type(lastName))
+
+# Combining strings / like-types of objects
+# String Conversion and Concatenation
+# ===========================================
+age = input("What is your age?\t")
+start = "My age is "
+sentence = start + str(age)
+print(sentence)
