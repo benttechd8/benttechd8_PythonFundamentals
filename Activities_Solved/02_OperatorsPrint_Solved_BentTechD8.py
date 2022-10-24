aeeeb381df26540a79061b083439865b930d6d21
benttechd8 <95981277+benttechd8@users.noreply.github.com>
2022-10-24 14:17:10 -0400

    Add files via upload
    Signed-off-by: benttechd8 <95981277+benttechd8@users.noreply.github.com>

---
+++ Activities_Solved/02_OperatorsPrint_Solved_BentTechD8.py
@@ -0,0 +1,62 @@
+#twitter @benttechd8
+#You got this!
+#Practice!
+#Bottom in seat, fingers on keys!
+
+
+# operators:
+# test your addition might, add 12 and 8:
+12 + 8
+# now store the sum in a variable called sum:
+sum = 12 + 8
+
+# test subtraction of 10 - 5:
+10 - 5
+# now store the remander in a variable called rem:
+rem = 10 - 5
+
+# test your multiplication, multiple sum * rem:
+sum * rem
+# store the above in a variable called total:
+total = sum * rem
+
+# test your division skills, divide total by 2:
+total / 2
+#store the quotient in a varialbe called quo:
+quo = total / 2
+
+# next up Modulus, modulo operation returns the remainder of a division
+# test your modulo skills, 33 modulus 2:
+33 % 2
+# Store the remainder (modulus) in a var called mod:
+mod = 33 % 2
+
+# Exponents, Let's test them too, use 2 to the exponent of 4 : 
+2 ** 4
+# Now store the output in a var called expon:
+expon = 2 ** 4
+
+# Floor Division, sometimes you to not need or want the remainder, use floor division for 33 floor divided by 2:
+33 // 2
+
+#Store it in a var called floor:
+floor = 33 // 2
+
+#print all the vars!
+# Example:
+
+#Now let's take a look at print and formatting some output:
+
+'''create two variables, test your print knowledge by leveraging % call formatting method
+, then calling on the vars directly, then move to f-string method!'''
+
+var1 = "test"
+var2 = 49
+#using %
+print("This is an int: %d and this is a string %s" % (var2, var1))
+
+#calling directly:
+print("This is an int:",var2,"and this is a string",var1)
+
+#f-string
+print(f"This an an int {var2} and this is a string {var1}")
